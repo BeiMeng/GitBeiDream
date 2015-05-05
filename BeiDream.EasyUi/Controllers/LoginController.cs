e7003267b59ee94bce5c9fe03b8b81eef3c15e98
BeiDream <331341164@qq.com>
2015-05-05 23:20:58 +0800

    登陆页面的设计

---
+++ BeiDream.EasyUi/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeiDream.EasyUi.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "建筑材料管理系统";
+            return View();
+        }
+
+    }
+}
