8ab6e6cbcc998a75139ae0849da7faf7f0782e6f
BeiDream <331341164@qq.com>
2015-05-12 17:29:26 +0800

    树形数据列表显示实现

--- BeiDream.EasyUi/Areas/Systems/Controllers/MenuManageController.cs
+++ BeiDream.EasyUi/Areas/Systems/Controllers/MenuManageController.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using BeiDream.EasyUi.Common;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Web;
 using System.Web.Mvc;
+using BeiDream.EasyUi.Areas.Systems.ViewModel;
+using BeiDream.Service.IService;
+using BeiDream.Service.PetaPoco.Service;
+using Util.Webs.EasyUi.Trees;
 
 namespace BeiDream.EasyUi.Areas.Systems.Controllers
 {
     public class MenuManageController : Controller
     {
+        //todo IOC依赖注入，Autofac
+        readonly IMenuService _menuService = new PetaPocoMenuService();
         //
         // GET: /Systems/MenuManage/
 
@@ -16,5 +23,31 @@ namespace BeiDream.EasyUi.Areas.Systems.Controllers
             return View();
         }
 
+        public ActionResult Query(QueryModel query)
+        {
+            List<ITreeNode> treeNodes = _menuService.GetNavigationMenu();
+            foreach (var treeNode in treeNodes)
+            {
+                treeNode.state = "closed";
+            }
+            PagedList<ITreeNode> result = new PagedList<ITreeNode>(treeNodes, query.Page, query.Rows);
+            return Json(new TreeGridResult { rows = result, total = result.TotalItemCount });
+        }
     }
+
+    public class TreeGridResult
+    {
+        public PagedList<ITreeNode> rows { get; set; }
+        public int total { get; set; }
+    }
+
+    public class QueryModel
+    {
+        public string Id { get; set; }
+        public int Page { get; set; }
+        public int Rows { get; set; }
+        public string Order { get; set; }
+        public string Sort { get; set; }
+    }
+
 }
---
+++ BeiDream.EasyUi/Areas/Systems/ViewModel/MenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using Util.Webs.EasyUi.Trees;
+
+namespace BeiDream.EasyUi.Areas.Systems.ViewModel
+{
+
+    public class MenuViewModel
+    {
+        [DataMember]
+        public string Id { get; set; }
+        /// <summary>
+        /// 文本
+        /// </summary>
+        [Required(ErrorMessage = "文本不能为空")]
+        [StringLength(50, ErrorMessage = "文本输入过长，不能超过50位")]
+        [Display(Name = "文本")]
+        [DataMember]
+        public string Text { get; set; }
+        /// <summary>
+        /// 拼音简码
+        /// </summary>
+        [Required(ErrorMessage = "拼音简码不能为空")]
+        [StringLength(200, ErrorMessage = "拼音简码输入过长，不能超过200位")]
+        [Display(Name = "拼音简码")]
+        [DataMember]
+        public string IconClass { get; set; }
+        [DataMember]
+        public string state { get; set; }
+    }
+}
